06558ee4470b936567ff357848e26fe55bdf82b1
karthikgreen1 <karthik.ramana25@gmail.com>
2025-11-20 14:56:25 +0530

    files are added

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'devtest-aurora-cluster.cluster-c2vsy6qc2hdm.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Admin123',
+    DB_DATABASE : 'webappdb'
+});
